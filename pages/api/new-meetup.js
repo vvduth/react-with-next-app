910f5f33c656a092fcdc67c5004d5a2718a18b42
Duc Thai <ducthai060501@gmail.com>
2022-01-01 14:31:59 +0200

    work with api routes

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,10 @@
+
+
+function handler(req,res) {
+    if (req.method === 'POST') {
+        const data = req.body ;
+        const {title, image, address, description } = data ;
+    }
+}
+
+export default handler ;
